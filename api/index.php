fab8687a2c708c5c355a43a0008b8477ba065a69
Akhyar Mubarok <asthesoto123@gmail.com>
2026-07-05 18:23:46 +0700

    cek perubahan gpt

--- api/index.php
+++ api/index.php
@@ -1,2 +1,23 @@
 <?php
-phpinfo();
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+if (!is_dir(storage_path('framework/cache'))) {
+    mkdir(storage_path('framework/cache'), 0777, true);
+}
+
+if (!is_dir(storage_path('framework/views'))) {
+    mkdir(storage_path('framework/views'), 0777, true);
+}
+
+if (!is_dir(storage_path('framework/sessions'))) {
+    mkdir(storage_path('framework/sessions'), 0777, true);
+}
+
+$app->handleRequest(Request::capture());
